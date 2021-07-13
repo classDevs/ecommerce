68f30d86dcdcd36098aaadb2830086e2cb8f7fce
RAHMANI Abd El Kader Seif El Islem <77025510+RAHAMNIabdelkaderseifelislem@users.noreply.github.com>
2021-07-13 12:08:15 +0100

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer" style="background-color: darkgray">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2021 Brought to You By <a href="#">Univ-saida</a></strong>
+    </div>
+</footer>
---
+++ readme.txt
@@ -0,0 +1,9 @@
+Customer Side
+Username: jude@yahoo.com
+
+Password: 123
+
+Admin Side
+Username: admin@admin.com
+
+Password: password
